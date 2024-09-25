5f28519b26390bfa5579565916a3848be2384353
Yoshiya Hinosawa <stibium121@gmail.com>
2024-09-25 20:35:00 +0900

    remove example scripts

--- fetch.ts
+++
@@ -1,8 +0,0 @@
-import http from "node:http"
-import { text } from "node:stream/consumers";
-
-const req = http.request("http://localhost:8000", { method: "POST" }, async (res) => {
-  console.log(await text(res));
-});
-req.write("hi");
-req.end();
--- serve.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  async fetch(req) {
-    const text = await req.text();
-    if (text) {
-      console.log(text);
-    }
-    return new Response("hi");
-  }
-}
