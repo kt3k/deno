cda1936d6377e6be424334b94e63126d6a9e007e
Yoshiya Hinosawa <stibium121@gmail.com>
2024-10-01 20:36:34 +0900

    reduce diff

--- ext/node/polyfills/http.ts
+++ ext/node/polyfills/http.ts
@@ -15,7 +15,7 @@ import { TextEncoder } from "ext:deno_web/08_text_encoding.js";
 import { setTimeout } from "ext:deno_web/02_timers.js";
 import {
   _normalizeArgs,
-  createConnection as netCreateConnection,
+  createConnection,
   ListenOptions,
   Socket,
 } from "node:net";
@@ -414,7 +414,7 @@ class ClientRequest extends OutgoingMessage {
         }
       } else {
         debug("CLIENT use net.createConnection", optsWithoutSignal);
-        this.onSocket(netCreateConnection(optsWithoutSignal));
+        this.onSocket(createConnection(optsWithoutSignal));
       }
     }
   }
@@ -696,6 +696,7 @@ class ClientRequest extends OutgoingMessage {
     }
     this.aborted = true;
     this.emit("abort");
+    //process.nextTick(emitAbortNT, this);
     this.destroy();
   }
 
